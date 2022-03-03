f4b92df9ad08df66ad163a1129d99f92e58b0712
melsa <melsa@MelannySalazar>
2022-03-02 19:00:17 -0600

    Creacion tarea 3

---
+++ src/test/java/com/tarea_3/demo/Tarea3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea_3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
